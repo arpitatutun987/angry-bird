4c260dac40c917da9426f4da98cd82d6ccb34290
arpitatutun987 <83904693+arpitatutun987@users.noreply.github.com>
2021-07-04 15:42:02 +0530

    Add files via upload

---
+++ Bird.js
@@ -0,0 +1,15 @@
+class Bird extends Base{
+
+    constructor(x,y){
+
+        super(x,y,85,85);
+        this.image = loadImage("bird.png");
+    }
+
+    display(){
+
+        super.display();
+
+      
+    }
+}
---
+++ Pig.js
@@ -0,0 +1,15 @@
+class Pig extends Base{
+
+    constructor(x,y){
+
+        super(x,y,85,85);
+        this.image = loadImage("enemy.png");
+    }
+
+    display(){
+
+        super.display();
+
+      
+    }
+}
